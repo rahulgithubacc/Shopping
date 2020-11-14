58ec47bbf8b8826fa03c32e218f781a48f0cd0c2
rahulqed <rahul.deshmukh324.rdr@gmail.com>
2020-11-14 07:53:16 +0530

    Fixed the shopping site errors

--- ShoppingSiteAutomation/src/test/java/com/practice/shopping/BaseTest.java
+++ ShoppingSiteAutomation/src/test/java/com/practice/shopping/BaseTest.java
@@ -7,8 +7,8 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
 public class BaseTest {
-    protected WebDriver driver;
-    protected WebDriverWait wait;
+    protected static WebDriver driver;
+    protected static WebDriverWait wait;
     @BeforeSuite
     public void beforeSuite() {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
