2b81ecfd30681901410cb2ec220e5d5ef15ae5f4
rahulqed <rahul.deshmukh324.rdr@gmail.com>
2020-11-16 08:56:48 +0530

    Started working on add to cart

--- ShoppingSiteAutomation/src/test/java/com/practice/shopping/BaseTest.java
+++ ShoppingSiteAutomation/src/test/java/com/practice/shopping/BaseTest.java
@@ -2,9 +2,7 @@ package com.practice.shopping;
 
 import com.practice.common.BrowserFactory;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterSuite;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
